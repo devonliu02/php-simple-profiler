b9c6ac5a933503b86c7943e34403762a81f74ad5
DevonLiu <devonliu02@gmail.com>
2017-04-26 20:07:17 +0800

    Added outputs

--- array_map.php
+++ array_map.php
@@ -22,3 +22,24 @@ dump('-----------------');
 
 $gender = ['m', 'f', 'm'];
 array_map('hi', $names, $gender);
+
+
+/*
+OUTPUT:
+
+bash-3.2$ php -v
+PHP 7.0.17 (cli) (built: Mar 18 2017 20:13:50) ( NTS )
+Copyright (c) 1997-2017 The PHP Group
+Zend Engine v3.0.0, Copyright (c) 1998-2017 Zend Technologies
+    with Xdebug v2.5.0, Copyright (c) 2002-2016, by Derick Rethans
+    
+bash-3.2$ php array_map.php 
+"Hi, Mr. Green"
+"Hi, ??? Black"
+"Hi, ??? Page"
+"-----------------"
+"Hi, Mr. Green"
+"Hi, Miss. Black"
+"Hi, Mr. Page"
+
+ */
